1603c5b1a889342a69ec388cc2860e25234f57dd
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-11-08 17:28:09 -0700

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -5,13 +5,17 @@ def replace(file,spelling):
     print("\n--- OUTPUT ---")
     count = 0
     for line in content:
+        Print_Count=False
         for key in spelling:
             if key in line:
                 count += 1
                 suggestion = line.split(key)
                 fill = spelling[key]
                 print(suggestion[0], fill, suggestion[1].replace("\n", ""))
+                Print_Count = True
                 miss.append(key)
+        if not Print_Count:
+            print(line.replace("\n",""))
 def suggest(file,spelling):
     content = file.readlines()
     miss=[]
@@ -19,19 +23,23 @@ def suggest(file,spelling):
     print("\n--- OUTPUT ---")
     count = 0
     for line in content:
+        Print_count = False
         for key in spelling:
             if key in line:
                 count+=1
                 suggestion = line.split(key)
                 fill = "("+str(count)+")"
                 print(suggestion[0],key,fill,suggestion[1].replace("\n",""))
+                Print_count = True
                 miss.append(key)
+        if not Print_count:
+            print(line.replace("\n",""))
     print("\n--- LEGEND ---")
     e=0
     for i in miss:
         e+=1
-        print("(",str(e),")",spelling[i])
-file_name = str(input("Enter a file name\n"))
+        print("("+str(e)+") "+spelling[i])
+file_name = str(input("Enter input file:\n"))
 file = open(file_name,'r')
 spelling = open('misspellings.txt','r')
 count = len(open("misspellings.txt").readlines())
